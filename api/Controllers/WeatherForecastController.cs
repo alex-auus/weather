890b97c280df229d1cdcd924f057443878012df3
Ali Almaktoum <ali.almaktoum@gmail.com>
2020-09-16 17:57:06 +1000

    styling and bindings

--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -33,7 +33,7 @@ namespace weather.Controllers
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)],
                 Humidity = 0.68,
-                Location = "Brisbanes",
+                Location = "Brisbane",
                 Status = "Sunny",
                 UVIndex = 0,
                 Visibility = 4
